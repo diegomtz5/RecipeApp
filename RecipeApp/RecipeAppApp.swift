8b53257ca6a37f41fe4429b01d9f93f9347adf32
Diego Martinez <diegosms45@gmail.com>
2025-05-05 12:24:04 -0700

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Diego Martinez on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
